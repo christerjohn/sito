1ffed5d23cfa3540fb4ed90d9791f2fd268e93df
ailenepequit <ailene.pequit@gmail.com>
2016-02-01 18:11:00 +0800

    updated code structure to conform with psr-2

--- src/Admission.php
+++ src/Admission.php
@@ -3,77 +3,69 @@
 
 namespace SITO\saleinvoice;
 
-
-
-
 use Person;
 
+class Admission extends Patient
+{
+	private $admission_no;
+	private $hospital_case_no;
+	private $medical_record_no;
+	private $chief_complaint;
+	private $date_admit;
+	private $date_discharge;	
 
+	public function setAdmission($admission_no) 
+	{
+		$this->admission_no = $admission_no;
+	}
 
-	class Admission extends Patient{
-		private $admission_no;
-		private $hospital_case_no;
-		private $medical_record_no;
-		private $chief_complaint;
-		private $date_admit;
-		private $date_discharge;	
-	
+	public function setHospitalCase($hospital_case_no) 
+	{
+		$this->hospital_case_no = $hospital_case_no;
+	}
 
-		public function setAdmission($admission_no) 
-		{
-			$this->admission_no = $admission_no;
-			
-		}
+	public function setMedicalRecord($medical_record_no) 
+	{
+		$this->medical_record_no = $medical_record_no;
+	}
 
-		public function setHospital_case($hospital_case_no) 
-		{
-			$this->hospital_case_no = $hospital_case_no;
-		
-		}
+	public function setChiefComplaint($chief_complaint) 
+	{
+		$this->chief_complaint = $chief_complaint;
+	}
 
-		public function setMedical_record($medical_record_no) 
-		{
-			$this->medical_record_no = $medical_record_no;
-		}
+	public function setDateAdmit($date_admit)
+	{
+		$this->date_admit = $date_admit;
+	}
 
-		public function setChief_complaint($chief_complaint) 
-		{
-			$this >chief_complaint = $chief_complaint;
-		}
+	public function getAdmissionNo()
+	{
+		return $this->admission_no;
+	}
 
-		public function setDate_admit($date_admit)
-		{
-			$this->date_admit = $date_admit;
-		}
+	public function getHospitalCaseNo()
+	{
+		return $this->hospital_case_no;
+	}
 
-		public function getAdmission_no()
-		{
-			 return $this-> admission_no;
-		}
+	public function getMedicalRecordNo()
+	{
+		return $this->medical_record_no;
+	}
 
-		public function getHospital_case_no()
-		{
-		 	return $this-> hospital_case_no;
-		}
+	public function getChiefComplaint()
+	{
+		return $this->chief_complaint;
+	}
 
-		public function getMedical_record_no()
-		{
-			 return $this-> medical_record_no;
+	public function getDateAdmit()
+	{
+		return $this->date_admit;
+	}
 
-		}
-
-		public function getChief_complaint(){
-
-			 return $this-> chief_complaint;
-		}
-
-		public function getDate_admit(){
-
-			 return $this-> date_admit;
-		}
-
-		public function getDate_discharge(){
-
-			 return $this-> date_discharge;
-		}	
+	public function getDateDischarge()
+	{
+		return $this->date_discharge;
 	}	
+}	
