a216fe09927b9c382fd719bc5c0ebf54692c393a
ailenepequit <ailene.pequit@gmail.com>
2016-02-01 18:03:11 +0800

    updated code structure to conform with psr-2

--- src/Patient.php
+++ src/Patient.php
@@ -1,56 +1,53 @@
 <?php
+
 namespace SITO\saleinvoice;
-
-
-
 
 use Person;
 
+class Patient extends Person{
 
-	 class Patient extends Person{
+	private $patient_no;
+	private $nationality;
+	private $occupation;
+	private $civil_status;
 
-		private $patient_no;
-		private $nationality;
-		private $occupation;
-		private $civil_status;
-
-
-		public function setPatient_no($patient_no) {
-			$this -> patient_no = $patient_no;
-			
-		}
-
-		public function setNationality($nationality) {
-			$this -> nationality = $nationality;
-		
-		}
-
-		public function setOccupation($occupation) {
-			$this -> occupation = $occupation;
-		}
-
-		public function setCivil_Status($civil_status) {
-			$this -> civil_status = $civil_status;
-		}
-
-		public function getPatient_no() {
-			return $this -> patient_no;
-		}
-
-		public function getNationality() {
-			return $this -> nationality;
-		}
-
-		public function getOccupation() {
-			return $this -> occupation;
-		}
-
-		public function getCivil_status() {
-			return $this -> civil_status;
-		}
-
+	public function setPatientNo($patient_no) 
+	{
+		$this->patient_no = $patient_no;
 	}
 
-		
-		
-?>
+	public function setNationality($nationality) 
+	{
+		$this->nationality = $nationality;
+	}
+
+	public function setOccupation($occupation) 
+	{
+		$this->occupation = $occupation;
+	}
+
+	public function setCivilStatus($civil_status) 
+	{
+		$this->civil_status = $civil_status;
+	}
+
+	public function getPatientNo() 
+	{
+		return $this->patient_no;
+	}
+
+	public function getNationality() 
+	{
+		return $this->nationality;
+	}
+
+	public function getOccupation() 
+	{
+		return $this->occupation;
+	}
+
+	public function getCivilStatus() 
+	{
+		return $this->civil_status;
+	}
+}
